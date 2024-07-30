f6326825f0b2b640e964789d520eea3fa462fd67
Hao Nguyen <hao.nguyenphong@nashtechglobal.com>
2024-07-30 14:49:55 +0700

    logging

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { SERVER_PORT } from './environments';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
